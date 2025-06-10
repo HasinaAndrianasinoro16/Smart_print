cc400eeeaaaadd9642972859e221b294892926c5
ANDRIANASINORO Hasina Arimanana <120100370+HasinaAndrianasinoro16@users.noreply.github.com>
2025-06-10 13:35:09 +0300

    Update Sousfacture.php

--- Smart_printBack/app/Models/Sousfacture.php
+++ Smart_printBack/app/Models/Sousfacture.php
@@ -4,8 +4,53 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Sousfacture extends Model
 {
+    protected $table = 'sousfacture';
+    protected $primaryKey = 'id';
+    protected $fillable = ['facture','description','quantite','prix_unitaire'];
+    public $timestamps = false;
+
+    //fonction pour creer une sous_facture
+    public static function create_sous_facture($facture,$description,$quantite,$prix_unitaire)
+    {
+        try {
+            $sousfacture = new Sousfacture();
+            $sousfacture->facture = $facture;
+            $sousfacture->description = $description;
+            $sousfacture->quantite = $quantite;
+            $sousfacture->prix_unitaire = $prix_unitaire;
+            $sousfacture->save();
+
+            return $sousfacture;
+        }catch (\Exception $exception){
+            throw new \Exception($exception->getMessage());
+        }
+    }
+
+    //fonction pour recuperer une sous facture a partir d'une facture
+    public static function get_sous_facture($facture)
+    {
+        try {
+            $sous_facture = DB::table('sousfacture')->where('facture',$facture)->get();
+            return $sous_facture;
+        }catch (\Exception $exception){
+            throw new \Exception($exception->getMessage());
+        }
+    }
+
+    //fonction pour recuperer les details factures d'une facture
+    public static function get_details_facture($facture)
+    {
+        try {
+            $details = DB::table('vue_detail_facture')->where('facture_id',$facture)->get();
+            return $details;
+        }catch (\Exception $exception){
+            throw new \Exception($exception->getMessage());
+        }
+    }
+
     use HasFactory;
 }
