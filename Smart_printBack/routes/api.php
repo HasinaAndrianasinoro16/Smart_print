63ce5ac024e2e70506bbef71990c00734f30dad0
ANDRIANASINORO Hasina Arimanana <120100370+HasinaAndrianasinoro16@users.noreply.github.com>
2025-06-21 16:27:44 +0300

    add boncommande api

--- Smart_printBack/routes/api.php
+++ Smart_printBack/routes/api.php
@@ -18,6 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//API Client
 Route::prefix('clients')->group(function () {
     Route::get('/',[\App\Http\Controllers\Client_controller::class,'get_all_Clients']);
     Route::get('/{id}',[\App\Http\Controllers\Client_controller::class,'get_Client']);
@@ -26,6 +27,7 @@ Route::prefix('clients')->group(function () {
     Route::put('/delete/{id}', [\App\Http\Controllers\Client_controller::class, 'destroy']);
 });
 
+//API factures
 Route::prefix('factures')->group(function () {
     Route::get('/',[\App\Http\Controllers\Facture_controller::class,'get_all_Factures']);
     Route::get('/{id}',[\App\Http\Controllers\Facture_controller::class,'get_facture']);
@@ -36,9 +38,17 @@ Route::prefix('factures')->group(function () {
     Route::get('/payer',[\App\Http\Controllers\Facture_controller::class,'get_all_facture_payer']);
 });
 
+//API sous facture
 Route::prefix('sousfactures')->group(function () {
     Route::get('/',[\App\Http\Controllers\Sousfacture_controller::class,'get_All_Sous_Factures']);
     Route::get('/{id_facture}',[\App\Http\Controllers\Sousfacture_controller::class,'get_sous_facture_with_facture']);
     Route::post('/add',[\App\Http\Controllers\Sousfacture_controller::class,'Form_add_Sous_Facture']);
     Route::get('/details/{facture_id}',[\App\Http\Controllers\Sousfacture_controller::class,'get_detail_sousfacture_by_facture']);
 });
+
+//API bon de commande
+Route::prefix('boncommandes')->group(function () {
+    Route::get('/{facture}',[\App\Http\Controllers\Boncommande_controller::class,'get_BonCommande_by_facture']);
+    Route::post('/add',[\App\Http\Controllers\Boncommande_controller::class,'form_BonCommande']);
+});
+
